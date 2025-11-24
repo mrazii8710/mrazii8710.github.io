98c495e18bd95dd0ab09632a2ef3b0e0667e6340
mrazii8710 <m.razii8710@gmail.com>
2025-11-24 05:46:16 +0300

    Create results.js
    This file can be updated weekly

---
+++ results.js
@@ -0,0 +1,31 @@
+const examResults = [
+    {
+        "sl": 1,
+        "name": "HABEEBU RAHIMAN KOTH.",
+        "whatsapp": "0534647345",
+        "place": "AL JUBAIL",
+        "score": 20
+    },
+    {
+        "sl": 2,
+        "name": "Misriya Dammam",
+        "whatsapp": "0565050717",
+        "place": "Dammam",
+        "score": 20
+    },
+    {
+        "sl": 3,
+        "name": "Rasheeda",
+        "whatsapp": "0540558086",
+        "place": "Dammam",
+        "score": 20
+    },
+    {
+        "sl": 4,
+        "name": "Test Student (Low Score)",
+        "whatsapp": "1234567890",
+        "place": "Riyadh",
+        "score": 15
+    }
+    // ADD NEW STUDENTS HERE (Don't forget the comma after the previous } )
+];
